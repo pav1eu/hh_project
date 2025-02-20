8cd5ca5cdde20fd87c5163fe84e003363081e43b
Pavel Artiev <arijev@bk.ru>
2025-02-20 14:55:35 +0300

    class JSONVacancy

--- src/hh_json.py
+++ src/hh_json.py
@@ -1,64 +1,66 @@
-import requests
+import json
+from abc import ABC
+from pathlib import Path
+from src.hh_vacancy import Vacancy
 
 
-class Vacancy:
-    __slots__ = ["name", "description", "salary_from", "salary_to", "url"]
-
-    def __init__(self, name, url, description=None, salary_from=None, salary_to=None):
-        self.name = name
-        self.description = description or "Описание не указано"
-        self.salary_from = salary_from
-        self.salary_to = salary_to
-        self.url = url
-
-        self.__validate()
-
-    def __validate(self):
-        if not self.name or not self.url:
-            raise ValueError("Эти аттрибуты обязательны")
-        if self.salary_from <= 0 or self.salary_to <= 0:
-            raise ValueError("Зарплата должна быть положительной")
-        if self.salary_from > self.salary_to:
-            raise ValueError("Минимальная зарплата должна быть больше максимальной")
-
-    def __str__(self):
-        return f"Вакансия: {self.name},\nОписание: {self.description},\nЗарплата: {self.salary_from} - {self.salary_to},\nURL: {self.url}"
-
-    def __lt__(self, other):
-        return (self.salary_from + self.salary_to) // 2 < (other.salary_from + other.salary_to) // 2
-
-    def __gt__(self,other):
-        return (self.salary_from + self.salary_to) // 2 > (other.salary_from + other.salary_to) // 2
+class VacancyFile(ABC):
 
     @staticmethod
-    def from_platform(platform_data):
-        """Метод для формирования списка вакансий из данных платформы"""
-        vacancies = []
-        for job_data in platform_data:
-            # Извлекаем необходимые данные из вложенных словарей с проверками на None
-            name = job_data.get("name", "Название не указано")
-            url = job_data.get("apply_alternate_url", "")  # Например, можно взять альтернативный URL отклика
+    def add_vacancies(self, vacancies):
+        pass
 
-            # Извлекаем данные по зарплате, добавляем проверку на None
-            salary_from = job_data.get("salary", {}).get("from", 0) if job_data.get("salary") else 0
-            salary_to = job_data.get("salary", {}).get("to", 0) if job_data.get("salary") else 0
+    @staticmethod
+    def get_vacancies(self, criteria):
+        pass
 
-            # Извлекаем описание из department с дополнительной проверкой на None
-            department = job_data.get("department")
-            description = department.get("name", "Описание не указано") if department else "Описание не указано"
+    @staticmethod
+    def delete_vacancies(self, criteria):
+        pass
 
-            # Создаем объект Vacancy с полученными данными
-            vacancy = Vacancy(
-                name=name, url=url, salary_from=salary_from, salary_to=salary_to, description=description
-            )
-            vacancies.append(vacancy)
-        return vacancies
+class JSONVacancy(VacancyFile):
+    def __init__(self, file_path="vacancies.json"):
+        self.__file_path = Path(file_path)
+        if not self.__file_path.exists():
+            self._save_data([])
 
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "description": self.description,
-            "salary_from": self.salary_from,
-            "salary_to": self.salary_to,
-            "url": self.url,
-        }
+    def _load_data(self):
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as f:
+                content = f.read().strip()
+                return json.loads(content) if content else []
+        except FileNotFoundError:
+            return []
+        except Exception as e:
+            print(f"Ошибка при чтении файла {e}")
+            return []
+
+    def _save_data(self, data):
+        try:
+            with open(self.__file_path, 'w', encoding='utf-8') as file:
+                json.dump(data, file, ensure_ascii=False, indent=4)
+        except Exception as e:
+            print(f"Ошибка при записи файла {e}")
+
+    def add_vacancies(self, vacancies):
+        data = self._load_data()
+        result = []
+        for vacancy in vacancies:
+            vacancy_dict = vacancy.to_dict()
+            if vacancy_dict not in data:
+                data.append(vacancy_dict)
+                result.append(vacancy)
+        self._save_data(data)
+
+    def get_vacancies(self, criteria):
+        data = self._load_data()
+        result = []
+        for item in data:
+            if all(item.get(key) == value for key, value in criteria.items()):
+                result.append(item)
+        return result
+
+    def delete_vacancies(self, criteria):
+        data = self._load_data()
+        data = [item for item in data if not all(item.get(key) == value for key, value in criteria.tem())]
+        self._save_data(data)
