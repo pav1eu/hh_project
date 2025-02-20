1efdffd428e58f89b01c399b84bfe9d30d22a180
Pavel Artiev <arijev@bk.ru>
2025-02-20 14:18:34 +0300

    class HeadHunterApi

---
+++ src/hh_api.py
@@ -0,0 +1,39 @@
+import requests
+from abc import ABC, abstractmethod
+
+
+class BaseApi(ABC):
+
+    @abstractmethod
+    def get_api(self):
+        pass
+
+    @abstractmethod
+    def get_vacancies(self, query, page = 1):
+        pass
+
+
+class HeadHunterApi(BaseApi):
+
+    def __init__(self, url="https://api.hh.ru/vacancies"):
+        self.__url = url
+
+    def get_api(self):
+        try:
+            response = requests.get(self.__url)
+            if response.status_code == 200:
+                return True
+        except requests.exceptions.RequestException as e:
+            print(f"Ошибка подключения {e}")
+            return False
+
+    def get_vacancies(self, query, page=10):
+        params = {"text": query, "per_page": page}
+        try:
+            response = requests.get(self.__url, params=params)
+            response.raise_for_status()
+            return response.json().get("items", [])
+        except Exception as e:
+            print(f"Неизвестная ошибка {e}")
+            return []
+
